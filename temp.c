73e6780bfb4b06783a760cd377b0e1bab54ef078
Artur Bondar <gamesshooter42@gmail.com>
2020-05-10 15:42:04 +0300

    simple improv

--- temp.c
+++ temp.c
@@ -1,17 +1,33 @@
 #include <stdio.h>
 
-#define MIN 0
-#define MAX 300
-#define STEP 10
-
-int conversion(int temp) {
-    return (temp * 9 / 5) + 32;
+int conversion(int temp, int value) {   
+    if (value == 0) 
+        return (temp * 9 / 5) + 32;
+    else if (value == 1) 
+        return (temp - 32) + 5 / 9;
+    else 
+        printf("Not one value did not match!\n");
+    
 }
 
 int main() {
-    int i;
+    int min, max, i, value;
+    
     printf("Cels in Fahr\n");
-    for( i = MIN; i <= MAX; i += STEP ) {
-        printf("%d - %d\n", i, conversion(i));
-    }
+    printf("Enter 1 for conversion Fahrenheit in Celsius or 0 for Celsius in Fahrenheit. \n");
+    scanf("%d", &value);
+    printf("Enter min value to max value temperature witch want to know\n");
+    printf("Min: ");
+    scanf("%d", &min);
+    printf("Max: ");
+    scanf("%d", &max);
+    if(value == 1)
+        printf("Fahr:\n");
+    else if(value == 0)
+        printf("Celsius:\n");
+
+    for(i = min; i <= max; i++)
+        printf("%d\n", conversion(i, value));
+
+    return 0;
 }
