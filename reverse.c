20eea86be5431c09105609263eb3e6dfba80d4f8
saitem <gamesshooter42@gmail.com>
2020-05-17 17:18:07 +0300

    function reverse

---
+++ reverse.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+#define MAXLINE 1000
+
+int getLine(char s[], int maxline);
+
+int main() {
+    int len, i;
+    char line[MAXLINE];
+
+    while((len = getLine(line, MAXLINE)) > 0) {    
+
+        for(i = len - 1; i >= 0; i--) 
+            printf("%c", line[i]);
+     
+    }
+}
+
+int getLine(char s[], int lim) {
+    int c, i;
+    for(i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; i++) 
+        s[i] = c;
+
+    if(c == '\n') {
+        s[i] = c;
+        ++i;
+    }
+
+    s[i] = '\0';
+    return i;
+}
