303087cb999ddc674a7211ef49a3361780c796d7
Artur Bondar <gamesshooter42@gmail.com>
2020-06-18 19:17:34 +0300

    ch

--- generateRandomString.c
+++ generateRandomString.c
@@ -2,25 +2,25 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define LENGTH 12
+#define LENGTH 13
 
 int myStrlen(char str[]);
 double myRandom(double min, double max);
 
 int main() {
-    char str[62] = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890";
+    char str[62] = "qwrtyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890";
     char arr[LENGTH];
     char temp[LENGTH];
 
     int i;
     srand(time(NULL));
     
-    for (i = 0; i < LENGTH; i++) {
-        arr[i] = myRandom(0, 1) * myStrlen(str);
-        *temp = str[arr[i]];
-        printf("%c", *temp);
+    for (i = 0; i < LENGTH - 1; i++) {
+        int j = myRandom(0, 1) * myStrlen(str);
+        temp[i] = str[j];
     }
-    printf("\n");
+    
+    printf("%s\n", temp);
 
     return 0;
 }
